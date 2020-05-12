aa00dac7910f71a892c1b1fc93fd002202b5fbdb
luutomas <61200017+luutomas@users.noreply.github.com>
2020-05-12 21:13:34 +0200

    Added egarch, ic_comp_table

--- Project.R
+++ Project.R
@@ -112,27 +112,26 @@ auto.arima(BABA_logret,ic ="bic", stepwise = FALSE)
 
 arima202 <- arima(BABA_logret, order = c(2,0,2))
 checkresiduals(arima202)
-arch.test(arima203)
+arch.test(arima202)
 
-arima203 <- arima(BABA_logret, order = c(2,0,3))
-checkresiduals(arima203)
-arch.test(arima203)
+arima000 <- arima(BABA_logret, order = c(0,0,0))
+checkresiduals(arima000)
+arch.test(arima000)
+#Ljung Box test not sufficient therefore model rejected
 
 # Homoskedastic residuals rejected => focus on conditional volatility => GARCH family models
 
 #### Conditional Volatility ####
-# We start with ARMA(2,0,2) - GARCH(1,1)
 # Save last 183 observations (6 months) for out of sample forecasting
 arma202_garch11_spec = ugarchspec(mean.model = list(armaOrder = c(2, 2)),
                                   variance.model = list(garchOrder = c(1, 1)))
-arma202_garch11 = ugarchfit(
-  spec = arma202_garch11_spec,
-  data = residuals(arima202),
-  out.sample = 183,
-  solver = "hybrid"
-)
-print(arma202_garch11)
-infocriteria(arma202_garch11)[2]
+arma202_garch11 = ugarchfit(spec = arma202_garch11_spec,data = residuals(arima202),
+                            out.sample = 183,solver = "hybrid"
+                          )
+
+arma202_garch11_fc <- ugarchforecast(arma202_garch11, n.roll = 183, n.ahead = 1)
+str(arma202_garch11_fc)
+
 # From the results, we can see that all coefficients except mean are significant 
 # and null hypothesis of no autocorrelation can not be rejected (LB test)
 
@@ -160,8 +159,8 @@ bestGARCH_AIC <- function(arima_model) {
   }
   return(c(best.p, best.q))
 }
-print(bestGARCH_AIC(arima203))
-print(bestGARCH_AIC(arima202))
+#print(bestGARCH_AIC(arima000))
+#print(bestGARCH_AIC(arima202))
 
 bestGARCH_BIC <- function(arima_model) {
   p.max <- 5
@@ -187,56 +186,318 @@ bestGARCH_BIC <- function(arima_model) {
   }
   return(c(best.p, best.q))
 }
-print(bestGARCH_BIC(arima203))
-print(bestGARCH_BIC(arima202))
+#print(bestGARCH_BIC(arima000))
+#print(bestGARCH_BIC(arima202))
 
-bestGARCHcombined <- function(ts_data) {
-  ap.max <- 5
-  aq.max <- 5
-  gp.max <- 5
-  gq.max <- 5
+bestGJRGARCH_AIC <- function(arima_model) {
+  p.max <- 5
+  q.max <- 5
   aic.min <- Inf
   best.p <- 0
   best.q <- 0
-  for (i1 in 1:ap.max) {
-    for (i2 in 1:aq.max) {
-      for (i3 in 1:gp.max) {
-        for (i4 in 1:gq.max) {
-          ourSpec <-
-            ugarchspec(
-              mean.model = list(
-                armaOrder = c(i1, i2),
-                include.mean = FALSE
-              ),
-              variance.model = list(garchOrder = c(i3, i4))
-            )
-          fit <-ugarchfit(spec = ourSpec, data = resid(arima(ts_data, order = c(i1, 0, i2))), solver = "hybrid")
-          inf.crit <- infocriteria(fit)[1]
-          aic.min <- ifelse(inf.crit < aic.min, inf.crit, aic.min)
-          
-          best.p <- ifelse(inf.crit == aic.min, i1, best.p)
-          best.q <- ifelse(inf.crit == aic.min, i2, best.q)
-        }
-      }
+  inf.crit <- 0
+  for (i1 in 1:p.max) {
+    for (i2 in 1:q.max) {
+      ourSpec <-
+        ugarchspec(
+          mean.model = list(armaOrder = c(0, 0), include.mean = FALSE),
+          variance.model = list(garchOrder = c(i1, i2), model = "gjrGARCH")
+        )
+      fit <- ugarchfit(spec = ourSpec, data = resid(arima_model), solver = "hybrid")
+      inf.crit <- infocriteria(fit)[1]
+      aic.min <- ifelse(inf.crit < aic.min, inf.crit, aic.min)
+      
+      best.p <- ifelse(inf.crit == aic.min, i1, best.p)
+      best.q <- ifelse(inf.crit == aic.min, i2, best.q)
     }
   }
   return(c(best.p, best.q))
 }
-print(bestGARCHcombined(BABA_logret))
+bestGJRGARCH_BIC <- function(arima_model) {
+  p.max <- 5
+  q.max <- 5
+  bic.min <- Inf
+  best.p <- 0
+  best.q <- 0
+  inf.crit <- 0
+  for (i1 in 1:p.max) {
+    for (i2 in 1:q.max) {
+      ourSpec <-
+        ugarchspec(
+          mean.model = list(armaOrder = c(0, 0), include.mean = FALSE),
+          variance.model = list(garchOrder = c(i1, i2), model = "gjrGARCH")
+        )
+      fit <- ugarchfit(spec = ourSpec, data = resid(arima_model), solver = "hybrid")
+      inf.crit <- infocriteria(fit)[2]
+      bic.min <- ifelse(inf.crit < bic.min, inf.crit, bic.min)
+      
+      best.p <- ifelse(inf.crit == bic.min, i1, best.p)
+      best.q <- ifelse(inf.crit == bic.min, i2, best.q)
+    }
+  }
+  return(c(best.p, best.q))
+}
 
+#print(bestGJRGARCH_AIC(arima203))
+#print(bestGJRGARCH_AIC(arima202))
+#print(bestGJRGARCH_BIC(arima203))
+#print(bestGJRGARCH_BIC(arima202))
+
+# We got that the best GARCH values are (1,1) and (1,4)
+bestARMA_GARCH_AIC <- function(ts_data) {
+  arma_p.max <- 5
+  arma_q.max <- 5
+  garch_p.max <- 5
+  garch_q.max <- 5
+  aic.min <- Inf
+  arma_best.p <- 0
+  arma_best.q <- 0
+  garch_best.p <- 0
+  garch_best.q <- 0
+  inf.crit <- 0
+  for (i1 in 1:arma_p.max) {
+    for (i2 in 1:arma_q.max) {
+      for (i3 in 1:garch_p.max) {
+        for (i4 in 1:garch_q.max) {
+          ourSpec <-ugarchspec(mean.model = list(armaOrder = c(i1, i2),include.mean = FALSE),
+                              variance.model = list(garchOrder = c(i3, i4))
+            )
+          fit <-ugarchfit(spec = ourSpec, data = ts_data, solver = "hybrid")
+          inf.crit <- infocriteria(fit)[1]
+          aic.min <- ifelse(inf.crit < aic.min, inf.crit, aic.min)
+          arma_best.p <- ifelse(inf.crit == aic.min, i3, arma_best.p)
+          arma_best.q <- ifelse(inf.crit == aic.min, i4, arma_best.q)
+          garch_best.p <- ifelse(inf.crit == aic.min, i3, garch_best.p)
+          garch_best.q <- ifelse(inf.crit == aic.min, i4, garch_best.q)
+        }
+      }
+    }
+  }
+  return(c(arma_best.p, arma_best.q, garch_best.p, garch_best.q))
+}
+bestARMA_GARCH_BIC <- function(ts_data) {
+  arma_p.max <- 5
+  arma_q.max <- 5
+  garch_p.max <- 5
+  garch_q.max <- 5
+  bic.min <- Inf
+  arma_best.p <- 0
+  arma_best.q <- 0
+  garch_best.p <- 0
+  garch_best.q <- 0
+  inf.crit <- 0
+  for (i1 in 1:arma_p.max) {
+    for (i2 in 1:arma_q.max) {
+      for (i3 in 1:garch_p.max) {
+        for (i4 in 1:garch_q.max) {
+          ourSpec <-ugarchspec(mean.model = list(armaOrder = c(i1, i2),include.mean = FALSE),
+                               variance.model = list(garchOrder = c(i3, i4))
+          )
+          fit <-ugarchfit(spec = ourSpec, data = ts_data, solver = "hybrid")
+          inf.crit <- infocriteria(fit)[2]
+          bic.min <- ifelse(inf.crit < bic.min, inf.crit, bic.min)
+          arma_best.p <- ifelse(inf.crit == bic.min, i3, arma_best.p)
+          arma_best.q <- ifelse(inf.crit == bic.min, i4, arma_best.q)
+          garch_best.p <- ifelse(inf.crit == bic.min, i3, garch_best.p)
+          garch_best.q <- ifelse(inf.crit == bic.min, i4, garch_best.q)
+        }
+      }
+    }
+  }
+  return(c(arma_best.p, arma_best.q, garch_best.p, garch_best.q))
+}
+
+#bestARMA_GARCH_AIC(BABA_logret) #1 5 1 5
+#bestARMA_GARCH_BIC(BABA_logret) #1 1 1 1
+
+bestARMA_GJRGARCH_AIC <- function(ts_data) {
+  arma_p.max <- 5
+  arma_q.max <- 5
+  garch_p.max <- 5
+  garch_q.max <- 5
+  aic.min <- Inf
+  arma_best.p <- 0
+  arma_best.q <- 0
+  garch_best.p <- 0
+  garch_best.q <- 0
+  inf.crit <- 0
+  for (i1 in 1:arma_p.max) {
+    for (i2 in 1:arma_q.max) {
+      for (i3 in 1:garch_p.max) {
+        for (i4 in 1:garch_q.max) {
+          ourSpec <-ugarchspec(mean.model = list(armaOrder = c(i1, i2),include.mean = FALSE),
+                               variance.model = list(garchOrder = c(i3, i4), model = "gjrGARCH")
+          )
+          fit <-ugarchfit(spec = ourSpec, data = ts_data, solver = "hybrid")
+          inf.crit <- infocriteria(fit)[1]
+          aic.min <- ifelse(inf.crit < aic.min, inf.crit, aic.min)
+          arma_best.p <- ifelse(inf.crit == aic.min, i3, arma_best.p)
+          arma_best.q <- ifelse(inf.crit == aic.min, i4, arma_best.q)
+          garch_best.p <- ifelse(inf.crit == aic.min, i3, garch_best.p)
+          garch_best.q <- ifelse(inf.crit == aic.min, i4, garch_best.q)
+        }
+      }
+    }
+  }
+  return(c(arma_best.p, arma_best.q, garch_best.p, garch_best.q))
+}
+bestARMA_GJRGARCH_BIC <- function(ts_data) {
+  arma_p.max <- 5
+  arma_q.max <- 5
+  garch_p.max <- 5
+  garch_q.max <- 5
+  bic.min <- Inf
+  arma_best.p <- 0
+  arma_best.q <- 0
+  garch_best.p <- 0
+  garch_best.q <- 0
+  inf.crit <- 0
+  for (i1 in 1:arma_p.max) {
+    for (i2 in 1:arma_q.max) {
+      for (i3 in 1:garch_p.max) {
+        for (i4 in 1:garch_q.max) {
+          ourSpec <-ugarchspec(mean.model = list(armaOrder = c(i1, i2),include.mean = FALSE),
+                               variance.model = list(garchOrder = c(i3, i4), model = "gjrGARCH")
+          )
+          fit <-ugarchfit(spec = ourSpec, data = ts_data, solver = "hybrid")
+          inf.crit <- infocriteria(fit)[2]
+          bic.min <- ifelse(inf.crit < bic.min, inf.crit, bic.min)
+          arma_best.p <- ifelse(inf.crit == bic.min, i3, arma_best.p)
+          arma_best.q <- ifelse(inf.crit == bic.min, i4, arma_best.q)
+          garch_best.p <- ifelse(inf.crit == bic.min, i3, garch_best.p)
+          garch_best.q <- ifelse(inf.crit == bic.min, i4, garch_best.q)
+        }
+      }
+    }
+  }
+  return(c(arma_best.p, arma_best.q, garch_best.p, garch_best.q))
+}
+
+#bestARMA_GJRGARCH_AIC(BABA_logret) # 1 1 1 1
+#bestARMA_GJRGARCH_BIC(BABA_logret) # 1 1 1 1
+
+bestARMA_eGARCH_AIC <- function(ts_data) {
+  arma_p.max <- 5
+  arma_q.max <- 5
+  garch_p.max <- 5
+  garch_q.max <- 5
+  aic.min <- Inf
+  arma_best.p <- 0
+  arma_best.q <- 0
+  garch_best.p <- 0
+  garch_best.q <- 0
+  inf.crit <- 0
+  for (i1 in 1:arma_p.max) {
+    for (i2 in 1:arma_q.max) {
+      for (i3 in 1:garch_p.max) {
+        for (i4 in 1:garch_q.max) {
+          ourSpec <-ugarchspec(mean.model = list(armaOrder = c(i1, i2),include.mean = FALSE),
+                               variance.model = list(garchOrder = c(i3, i4), model = "eGARCH")
+          )
+          fit <-ugarchfit(spec = ourSpec, data = ts_data, solver = "hybrid")
+          inf.crit <- infocriteria(fit)[1]
+          aic.min <- ifelse(inf.crit < aic.min, inf.crit, aic.min)
+          arma_best.p <- ifelse(inf.crit == aic.min, i3, arma_best.p)
+          arma_best.q <- ifelse(inf.crit == aic.min, i4, arma_best.q)
+          garch_best.p <- ifelse(inf.crit == aic.min, i3, garch_best.p)
+          garch_best.q <- ifelse(inf.crit == aic.min, i4, garch_best.q)
+        }
+      }
+    }
+  }
+  return(c(arma_best.p, arma_best.q, garch_best.p, garch_best.q))
+}
+bestARMA_eGARCH_BIC <- function(ts_data) {
+  arma_p.max <- 5
+  arma_q.max <- 5
+  garch_p.max <- 5
+  garch_q.max <- 5
+  bic.min <- Inf
+  arma_best.p <- 0
+  arma_best.q <- 0
+  garch_best.p <- 0
+  garch_best.q <- 0
+  inf.crit <- 0
+  for (i1 in 1:arma_p.max) {
+    for (i2 in 1:arma_q.max) {
+      for (i3 in 1:garch_p.max) {
+        for (i4 in 1:garch_q.max) {
+          ourSpec <-ugarchspec(mean.model = list(armaOrder = c(i1, i2),include.mean = FALSE),
+                               variance.model = list(garchOrder = c(i3, i4), model = "eGARCH")
+          )
+          fit <-ugarchfit(spec = ourSpec, data = ts_data, solver = "hybrid")
+          inf.crit <- infocriteria(fit)[2]
+          bic.min <- ifelse(inf.crit < bic.min, inf.crit, bic.min)
+          arma_best.p <- ifelse(inf.crit == bic.min, i3, arma_best.p)
+          arma_best.q <- ifelse(inf.crit == bic.min, i4, arma_best.q)
+          garch_best.p <- ifelse(inf.crit == bic.min, i3, garch_best.p)
+          garch_best.q <- ifelse(inf.crit == bic.min, i4, garch_best.q)
+        }
+      }
+    }
+  }
+  return(c(arma_best.p, arma_best.q, garch_best.p, garch_best.q))
+}
+
+bestARMA_eGARCH_AIC(BABA_logret) # 5 3 5 3
+bestARMA_eGARCH_BIC(BABA_logret) # 4 3 4 3
+
+
+#
 arma202_garch11_spec <- ugarchspec(mean.model = list(armaOrder = c(2, 2)),
                                   variance.model = list(garchOrder = c(1, 1)))
 arma202_garch11 <- ugarchfit(spec = arma202_garch11_spec, data = residuals(arima202),
                              out.sample = 183,solver = "hybrid")
-print(arma202_garch11)
-arma202_garch11_res <- residuals(arma202_garch11)
-acf(arma202_garch11_res)
-plot(arma202_garch11_res)
-Box.test(arma202_garch11_res)
-hist(arma202_garch11_res)
 
+arma202_garch14_spec <- ugarchspec(mean.model = list(armaOrder = c(2, 2)),
+                                   variance.model = list(garchOrder = c(1, 4)))
+arma202_garch14 <- ugarchfit(spec = arma202_garch14_spec, data = residuals(arima202),
+                             out.sample = 183,solver = "hybrid")
+
+arma000_garch11_spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0)),
+                                   variance.model = list(garchOrder = c(1, 1)))
+arma000_garch11 <- ugarchfit(spec = arma000_garch11_spec, data = residuals(arima000),
+                             out.sample = 183,solver = "hybrid")
+
+arma000_garch14_spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0)),
+                                   variance.model = list(garchOrder = c(1, 4)))
+arma000_garch14 <- ugarchfit(spec = arma000_garch14_spec, data = residuals(arima000),
+                             out.sample = 183,solver = "hybrid")
+
+arma105_garch15_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 5)),
+                                   variance.model = list(garchOrder = c(1, 5)))
+arma105_garch15 <- ugarchfit(spec = arma105_garch15_spec, data = BABA_logret,
+                             out.sample = 183,solver = "hybrid")
+
+arma101_garch11_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 1)),
+                                   variance.model = list(garchOrder = c(1, 1)))
+arma101_garch11 <- ugarchfit(spec = arma101_garch11_spec, data = BABA_logret,
+                             out.sample = 183,solver = "hybrid")
+
+arma101_gjrgarch11_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 1)),
+                                      variance.model = list(garchOrder = c(1, 1), model  = "gjrGARCH"))
+arma101_gjrgarch11 <- ugarchfit(spec = arma101_gjrgarch11_spec, data = BABA_logret,
+                             out.sample = 183)
+
+arma503_egarch53_spec <- ugarchspec(mean.model = list(armaOrder = c(5, 3)),
+                                      variance.model = list(garchOrder = c(5, 3), model  = "eGARCH"))
+arma503_egarch53 <- ugarchfit(spec = arma503_egarch53_spec, data = BABA_logret,
+                                out.sample = 183, solver = "hybrid")
+
+arma403_egarch43_spec <- ugarchspec(mean.model = list(armaOrder = c(4, 3)),
+                                    variance.model = list(garchOrder = c(4, 3), model  = "eGARCH"))
+arma403_egarch43 <- ugarchfit(spec = arma403_egarch43_spec, data = BABA_logret,
+                              out.sample = 183, solver = "hybrid")
+
+ic_comp_table <- data.frame(cbind(infocriteria(arma000_garch11), infocriteria(arma000_garch14), 
+                                  infocriteria(arma202_garch11), infocriteria(arma202_garch14), 
+                                  infocriteria(arma105_garch15), infocriteria(arma101_garch11),
+                                  infocriteria(arma101_gjrgarch11),
+                                  infocriteria(arma503_egarch53), infocriteria(arma403_egarch43)))
+names(ic_comp_table) <- c("000-11", "000-14", "202-11", "202-14", "105-15", "101-11", "101-11gjr", "503-53e", "403-43e")
+ic_comp_table
 # Testing normality 
-jarque.bera.test(residuals(arma202_garch11))
+jarque.bera.test(arma202_garch11_res)
 par(mfrow = c(1, 2))
 hist(residuals(arma202_garch11), breaks = 30, main ='Histogram', cex.main = 0.8, cex.lab = 0.8, xlab = NA,
      cex.axis = 0.8)
